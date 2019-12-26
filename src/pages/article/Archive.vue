571c41bd868291a472dfa4d20bda0a784175ce72
dionylon <dionylong@163.com>
2019-12-26 22:43:40 +0800

    fix

--- src/pages/article/Archive.vue
+++ src/pages/article/Archive.vue
@@ -9,7 +9,7 @@
         subtitle="2019-12-26"
         side="left"
       >
-        <div>
+        <div class="ellipsis-3-lines">
           文章 {{i}}<br/>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
